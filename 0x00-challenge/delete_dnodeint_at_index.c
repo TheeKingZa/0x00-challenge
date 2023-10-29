7bfdd81b8298a11e431233a4d5e5b43fd218b446
Pule <proceedingmc@gmail.com>
2023-10-29 12:38:25 +0200

    just finished debuging the and did a few edits few github for edits

--- 0x00-challenge/delete_dnodeint_at_index.c
+++ 0x00-challenge/delete_dnodeint_at_index.c
@@ -11,39 +11,41 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    if (*head == NULL)
-        return (-1);
+	dlistint_t *current;
+	unsigned int i;
 
-    dlistint_t *current = *head;
+	if (*head == NULL)
+		return (-1);
+	current = *head;
 
-    if (index == 0)
-    {
-        /* Update the head to the next node and handle next's prev */
-        *head = current->next;
-        if (current->next)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		/* Update the head to the next node and handle next's prev */
+		*head = current->next;
 
-    unsigned int i = 0;
-    while (i < index)
-    {
-        if (current == NULL)
-            return (-1);  /* Index is out of bounds */
-        current = current->next;
-        i++;
-    }
+		if (current->next)
+			current->next->prev = NULL;
+		free(current);
+		return (1);
+	}
+	
+	i = 0;
+	
+	while (i < index)
+	{
+		if (current == NULL)
+			return (-1);  /* Index is out of bounds */
+		current = current->next;
+		i++;
+	}
+	if (current == NULL)
+		return (-1);  /* Index is out of bounds */
 
-    if (current == NULL)
-        return (-1);  /* Index is out of bounds */
+	/* Update the previous node's next and handle next's prev */
+	current->prev->next = current->next;
+	if (current->next)
+		current->next->prev = current->prev;
 
-    /* Update the previous node's next and handle next's prev */
-    current->prev->next = current->next;
-    if (current->next)
-        current->next->prev = current->prev;
-
-    free(current);
-
-    return (1);
+	free(current);
+	return (1);
 }
